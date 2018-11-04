898f9ebaedfa5e12bcba6d4a22c2048ca0873ffd
bglee <deptno@gmail.com>
2018-11-04 18:39:44 +0900

    height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import 'tachyons/css/tachyons.min.css'
 
 export const Layout = props => (
-  <div id="layout" className={'bp3-dark pa2'} style={{backgroundColor: 'rgb(55,72,87)'}}>
+  <div id="layout" className={'bp3-dark pa2'} style={{backgroundColor: 'rgb(55,72,87)', height: '100%', minHeight: '100vh'}}>
     <Head/>
     <Header/>
     <main>
