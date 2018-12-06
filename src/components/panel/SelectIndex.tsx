e0b8c3719a1e3077653652a0c7632f74ac5be800
bglee <deptno@gmail.com>
2018-12-07 02:10:55 +0900

    index

--- src/components/panel/SelectIndex.tsx
+++ src/components/panel/SelectIndex.tsx
@@ -1,35 +1,41 @@
-import React, {Component} from 'react'
+import React, {FunctionComponent} from 'react'
 import classnames from 'classnames'
 import {Select} from '../Select'
 import {actions} from '../../redux/dynamon'
 import {connect} from 'react-redux'
 import {RootState} from '../../redux'
 
-export class SelectIndexComponent extends Component<Props> {
-  render() {
-    const {indexes} = this.props
+export const SelectIndexComponent: FunctionComponent<Props> = props => {
+  const {indexes} = props
 
-    return <div className="ph2">
-      <Select title="Indexes" onChange={console.log} default="default" className="mb2" disabled>
-        <option value="default">Default Index</option>
-        {indexes.map(idx =>
-          <option value={idx.IndexArn}>{idx.IndexName} ({idx.KeySchema.map(k => k.AttributeName).join(', ')})</option>
-        )}
-      </Select>
-      <div className="bp3-button-group bp3-align-right bp3-minimal">
-        <button
-          type="button"
-          className={classnames('bp3-button bp3-icon-add bp3-minimal bp3-intent-success')}
-          disabled
-        />
-        <button
-          type="button"
-          className={classnames('bp3-button bp3-icon-remove bp3-minimal bp3-intent-danger')}
-          disabled
-        />
-      </div>
+  return <div className="ph2">
+    <Select
+      title="Indexes"
+      onChange={e => alert(`${e.target.value}\n@todo`)}
+      default="__"
+      className="mb2"
+      disabled={indexes.length === 0}
+    >
+      <option value="__">Default Index</option>
+      {indexes.map(idx =>
+        <option key={idx.IndexArn} value={idx.IndexArn}>
+          {idx.IndexName} ({idx.KeySchema.map(k => k.AttributeName).join(', ')})
+        </option>
+      )}
+    </Select>
+    <div className="bp3-button-group bp3-align-right bp3-minimal">
+      <button
+        type="button"
+        className={classnames('bp3-button bp3-icon-add bp3-minimal bp3-intent-success')}
+        disabled
+      />
+      <button
+        type="button"
+        className={classnames('bp3-button bp3-icon-remove bp3-minimal bp3-intent-danger')}
+        disabled
+      />
     </div>
-  }
+  </div>
 }
 const mapStateToProps = (state: RootState) => state.dynamon
 const mapDispatchToProps = actions
