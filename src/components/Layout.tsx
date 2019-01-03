e4c1c3bff1217b82b78ccc5594dab85481785227
bglee <deptno@gmail.com>
2019-01-03 13:27:46 +0900

    has error #13

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import {Header} from './Header'
-import {Footer} from './Footer'
-import Head from 'next/head'
 import 'normalize.css/normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
@@ -9,7 +7,6 @@ import 'tachyons/css/tachyons.min.css'
 
 export const Layout = props => (
   <div id="layout" className={'bp3-dark pa2'} style={{backgroundColor: 'rgb(55,72,87)', height: '100%', minHeight: '100vh'}}>
-    <Head/>
     <Header/>
     <main>
       {props.children}
